f91581512f88e21eb68b76964b575aee1018ed7f
James Devine <j.devine@lancaster.ac.uk>
2020-02-12 14:56:19 +0000

    tweak visualise.py to correct wave forms for 1mbit

--- visualise.py
+++ visualise.py
@@ -3,20 +3,21 @@ import numpy as np # to work with numerical data efficiently
 
 def shift(array,num):
     pres = len(array)
-    new = np.array([0] * num)
+    new = np.array([0] * int(num))
     array = np.concatenate((new,array))
     return array[:pres]
 
-fs = 500 # sample rate
-f = 2.4 # the frequency of the signal
-f2 = 0.025
-f3 = 1
+def neg_shift(array, shift):
+    return np.concatenate((array[abs(shift):],array[:abs(shift)]))
+
+fs = 1000 # number of samples
+cycle_time = 62.5
+f_mult = 2
 
 x = np.arange(fs) # the points on the x axis for plotting
 # compute the value (amplitude) of the sin wave at the for each sample
-y = np.sin(10 * np.pi * f * (x/fs))
-y2 = np.sin(10 * np.pi * f3 * (x/fs))
-y3 = shift(np.sin(10 * np.pi * f3 * (x/fs)),150)
+y = np.sin(f_mult * (2 * np.pi) * (x/fs)) #0
+# y = np.sin(10 * np.pi * .5 * (x/fs)) #1
 
-plt.plot(x,y2, x,y, x, y3)
+plt.plot(x,y,x,shift(y, cycle_time * 3.0))#,x,shift(y, sf * 3.),x,shift(y, sf * 4.))
 plt.show()
