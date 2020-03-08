2c00e24040a93d0b5b09c0e234e2925376a96251
James Devine <j.devine@lancaster.ac.uk>
2020-03-08 19:59:19 +0000

    add script to create mock wave.

---
+++ visualise_wave_triple.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt # For ploting
+import numpy as np # to work with numerical data efficiently
+import pandas as pd
+
+def shift(array,num):
+    pres = len(array)
+    print("SHIFT BY", int(num))
+    new = np.array([0] * int(num))
+    array = np.concatenate((new,array))
+    return array[:pres]
+
+def neg_shift(array, shift):
+    return np.concatenate((array[abs(shift):],array[:abs(shift)]))
+
+fs = 1000 # number of samples
+cycle_time = 62.5
+f_mult = 2
+
+x = np.arange(fs) # the points on the x axis for plotting
+# compute the value (amplitude) of the sin wave at the for each sample
+y = np.sin(f_mult * (2 * np.pi) * (x/fs)) #0
+y2 = np.sin(1 * (2 * np.pi) * (x/fs)) #0
+# y = np.sin(10 * np.pi * .5 * (x/fs)) #1
+
+xt = [i * 100 for i in range(0,11)]
+
+plt.subplot(3,1,1)
+plt.plot(x,y, 'r')#,x,shift(y, sf * 3.),x,shift(y, sf * 4.))
+plt.ylabel("Phase")
+plt.xlabel("Time (nanoseconds)")
+plt.subplot(3,1,2)
+plt.plot(x,shift(y, cycle_time * 3.),'g')
+plt.ylabel("Phase")
+plt.xlabel("Time (nanoseconds)")
+plt.subplot(3,1,3)
+comb = np.add(y, np.array(shift(y, cycle_time * 3.)))
+plt.plot(x, comb,'b')
+# plt.subplot(x,y2)
+# plt.xticks(xt)
+plt.ylabel("Phase")
+plt.xlabel("Time (nanoseconds)")
+# plt.set_ylabel("Phase")
+# plt.set_xlabel("Time (nanoseconds)")
+plt.show()
